adda30e64c2a83d33dba7c7fe609b9b638ad2253
marha <marha@users.sourceforge.net>
2009-09-16 14:26:14 +0000

    synchronised include\dix-config.h and xorg-server\include\dix-config.h.in

--- include/dix-config.h
+++ include/dix-config.h
@@ -1,4 +1,3 @@
-/* include/dix-config.h.  Generated from dix-config.h.in by configure.  */
 /* dix-config.h.in: not at all generated.                      -*- c -*- */
 
 #ifndef _DIX_CONFIG_H_
@@ -33,14 +32,6 @@
 /* Support Composite Extension */
 #define COMPOSITE 1
 
-/* Define to one of `_getb67', `GETB67', `getb67' for Cray-2 and Cray-YMP
-   systems. This function is required for `alloca.c' support on those systems.
-   */
-/* #undef CRAY_STACKSEG_END */
-
-/* Define to 1 if using `alloca.c'. */
-/* #undef C_ALLOCA */
-
 /* Support Damage extension */
 #define DAMAGE 1
 
@@ -66,16 +57,13 @@
 #define DPMSExtension 1
 
 /* Build GLX extension */
-/*#define GLXEXT 1 */
+#undef GLXEXT
 
 /* Build GLX DRI loader */
-/* #undef GLX_DRI */
+#undef GLX_DRI
 
 /* Path to DRI drivers */
 #define DRI_DRIVER_PATH ""
-
-/* Include handhelds.org h3600 touchscreen driver */
-/* #undef H3600_TS */
 
 /* Support XDM-AUTH*-1 */
 #define HASXDMAUTH 1
@@ -84,19 +72,19 @@
 #define HAS_GETDTABLESIZE 1
 
 /* Define to 1 if you have the `getifaddrs' function. */
-/* #undef HAS_GETIFADDRS */
+#undef HAS_GETIFADDRS
 
 /* Define to 1 if you have the `getpeereid' function. */
-/*#define HAS_GETPEEREID 1*/
+#undef HAS_GETPEEREID
 
 /* Define to 1 if you have the `getpeerucred' function. */
-/* #undef HAS_GETPEERUCRED */
+#undef HAS_GETPEERUCRED
 
 /* Define to 1 if you have the `mmap' function. */
-//#define HAS_MMAP 1
+#undef HAS_MMAP
 
 /* Support SHM */
-//MH#define HAS_SHM 1
+#undef HAS_SHM
 
 /* Define to 1 if you have `alloca', as a function or macro. */
 #define HAVE_ALLOCA 1
@@ -105,20 +93,23 @@
    */
 #define HAVE_ALLOCA_H 1
 
+/* Have the 'strlcpy' function */
+#undef HAS_STRLCPY
+
 /* Define to 1 if you have the <asm/mtrr.h> header file. */
-/* #undef HAVE_ASM_MTRR_H */
+#undef HAVE_ASM_MTRR_H
 
 /* Has backtrace support */
-/* #undef HAVE_BACKTRACE */
+#undef HAVE_BACKTRACE
 
 /* Define to 1 if you have the <byteswap.h> header file. */
 #define HAVE_BYTESWAP_H 1
 
 /* Define to 1 if you have cbrt */
-//#define HAVE_CBRT 1
+#undef HAVE_CBRT
 
 /* Define to 1 if you have the <dbm.h> header file. */
-/* #undef HAVE_DBM_H */
+#undef HAVE_DBM_H
 
 /* Define to 1 if you have the <dirent.h> header file, and it defines `DIR'.
    */
@@ -128,10 +119,10 @@
 #define HAVE_DLFCN_H 1
 
 /* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
-/* #undef HAVE_DOPRNT */
+#undef HAVE_DOPRNT
 
 /* Have execinfo.h */
-/* #undef HAVE_EXECINFO_H */
+#undef HAVE_EXECINFO_H
 
 /* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
@@ -140,7 +131,7 @@
 #define HAVE_GETEUID 1
 
 /* Define to 1 if you have the `getisax' function. */
-/* #undef HAVE_GETISAX */
+#undef HAVE_GETISAX
 
 /* Define to 1 if you have the `getopt' function. */
 #define HAVE_GETOPT 1
@@ -152,16 +143,22 @@
 #define HAVE_GETUID 1
 
 /* Define to 1 if you have the `getzoneid' function. */
-/* #undef HAVE_GETZONEID */
+#undef HAVE_GETZONEID
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define to 1 if you have version 2.2 (or newer) of the drm library */
-/* #undef HAVE_LIBDRM_2_2 */
-
 /* Have Quartz */
-/* #undef XQUARTZ */
+#undef XQUARTZ
+
+/* Support application updating through sparkle. */
+#undef XQUARTZ_SPARKLE
+
+/* Prefix to use for launchd identifiers */
+#undef LAUNCHD_ID_PREFIX
+
+/* Build a standalone xpbproxy */
+#undef STANDALONE_XPBPROXY
 
 /* Define to 1 if you have the `m' library (-lm). */
 #define HAVE_LIBM 1
@@ -170,16 +167,13 @@
 #define HAVE_LINK 1
 
 /* Define to 1 if you have the <linux/agpgart.h> header file. */
-/* #undef HAVE_LINUX_AGPGART_H */
+#undef HAVE_LINUX_AGPGART_H
 
 /* Define to 1 if you have the <linux/apm_bios.h> header file. */
-/* #undef HAVE_LINUX_APM_BIOS_H */
+#undef HAVE_LINUX_APM_BIOS_H
 
 /* Define to 1 if you have the <linux/fb.h> header file. */
-/* #undef HAVE_LINUX_FB_H */
-
-/* Define to 1 if you have the <linux/h3600_ts.h> header file. */
-/* #undef HAVE_LINUX_H3600_TS_H */
+#undef HAVE_LINUX_FB_H
 
 /* Define to 1 if you have the `memmove' function. */
 #define HAVE_MEMMOVE 1
@@ -194,19 +188,22 @@
 #define HAVE_MKSTEMP 1
 
 /* Define to 1 if you have the <ndbm.h> header file. */
-/* #undef HAVE_NDBM_H */
+#undef HAVE_NDBM_H
 
 /* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
-/* #undef HAVE_NDIR_H */
+#undef HAVE_NDIR_H
 
 /* Define to 1 if you have the <rpcsvc/dbm.h> header file. */
-/* #undef HAVE_RPCSVC_DBM_H */
+#undef HAVE_RPCSVC_DBM_H
+
+/* Define to use libmd SHA1 functions instead of OpenSSL libcrypto */
+#undef HAVE_SHA1_IN_LIBMD
 
 /* Define to 1 if you have the `shmctl64' function. */
-/* #undef HAVE_SHMCTL64 */
+#undef HAVE_SHMCTL64
 
 /* Define to 1 if you have the <stdint.h> header file. */
-/*#define HAVE_STDINT_H 1*/
+#undef HAVE_STDINT_H
 
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
@@ -227,21 +224,21 @@
 #define HAVE_STRTOL 1
 
 /* Define to 1 if SYSV IPC is available */
-/* #undef HAVE_SYSV_IPC */
+#undef HAVE_SYSV_IPC
 
 /* Define to 1 if you have the <sys/agpio.h> header file. */
-/* #undef HAVE_SYS_AGPIO_H */
+#undef HAVE_SYS_AGPIO_H
 
 /* Define to 1 if you have the <sys/dir.h> header file, and it defines `DIR'.
    */
-/* #undef HAVE_SYS_DIR_H */
+#undef HAVE_SYS_DIR_H
 
 /* Define to 1 if you have the <sys/io.h> header file. */
-/* #undef HAVE_SYS_IO_H */
+#undef HAVE_SYS_IO_H
 
 /* Define to 1 if you have the <sys/ndir.h> header file, and it defines `DIR'.
    */
-/* #undef HAVE_SYS_NDIR_H */
+#undef HAVE_SYS_NDIR_H
 
 /* Define to 1 if you have the <sys/stat.h> header file. */
 #define HAVE_SYS_STAT_H 1
@@ -250,37 +247,37 @@
 #define HAVE_SYS_TYPES_H 1
 
 /* Define to 1 if you have the <sys/vm86.h> header file. */
-/* #undef HAVE_SYS_VM86_H */
+#undef HAVE_SYS_VM86_H
 
 /* Define to 1 if you have the <tslib.h> header file. */
-/* #undef HAVE_TSLIB_H */
+#undef HAVE_TSLIB_H
 
 /* Define to 1 if you have the <unistd.h> header file. */
-/*#define HAVE_UNISTD_H 1*/
+#undef HAVE_UNISTD_H
 
 /* Have /dev/urandom */
-/* #undef HAVE_URANDOM */
+#undef HAVE_URANDOM
 
 /* Define to 1 if you have the `vprintf' function. */
 #define HAVE_VPRINTF 1
 
 /* Support IPv6 for TCP connections */
-/* #undef IPv6 */
+#undef IPv6
 
 /* Support os-specific local connections */
-/* #undef LOCALCONN */
+#undef LOCALCONN
 
 /* Support MIT Misc extension */
 #define MITMISC 1
 
 /* Support MIT-SHM Extension */
-//#define MITSHM 1
+#undef MITSHM
 
 /* Disable some debugging code */
 #define NDEBUG 1
 
 /* Enable some debugging code */
-/* #undef DEBUG */
+#undef DEBUG
 
 /* Name of package */
 #define PACKAGE "xorg-server"
@@ -307,10 +304,7 @@
 #define SCREENSAVER 1
 
 /* Support Secure RPC ("SUN-DES-1") authentication for X11 clients */
-/* #undef SECURE_RPC */
-
-/* Use a lock to prevent multiple servers on a display */
-/* #define SERVER_LOCK 1 */
+#undef SECURE_RPC
 
 /* Support SHAPE extension */
 #define SHAPE 1
@@ -330,25 +324,25 @@
 #define STDC_HEADERS 1
 
 /* Define to 1 on systems derived from System V Release 4 */
-/* #undef SVR4 */
+#undef SVR4
 
 /* Support TCP socket connections */
 #define TCPCONN 1
 
 /* Enable touchscreen support */
-/* #undef TOUCHSCREEN */
+#undef TOUCHSCREEN
 
 /* Support tslib touchscreen abstraction library */
-/* #undef TSLIB */
+#undef TSLIB
 
 /* Support UNIX socket connections */
-/*#define UNIXCONN 1*/
+#undef UNIXCONN
 
 /* Define to use byteswap macros from <sys/endian.h> */
-/* #undef USE_SYS_ENDIAN_H */
+#undef USE_SYS_ENDIAN_H
 
 /* unaligned word accesses behave as expected */
-/* #undef WORKING_UNALIGNED_INT */
+#undef WORKING_UNALIGNED_INT
 
 /* Build X string registry */
 #define XREGISTRY 1
@@ -357,13 +351,13 @@
 #define XACE 1
 
 /* Build SELinux extension */
-/* #undef XSELINUX */
+#undef XSELINUX
 
 /* Support XCMisc extension */
 #define XCMISC 1
 
 /* Build Security extension */
-/* #undef XCSECURITY */
+#undef XCSECURITY
 
 /* Support Xdmcp */
 #define XDMCP 1
@@ -372,19 +366,16 @@
 #define XEVIE 1
 
 /* Build XFree86 BigFont extension */
-/* #undef XF86BIGFONT */
-
-/* Support XFree86 miscellaneous extensions */
-/* #undef XF86MISC */
+#undef XF86BIGFONT
 
 /* Support XFree86 Video Mode extension */
-/* #undef XF86VIDMODE */
+#undef XF86VIDMODE
 
 /* Support XFixes extension */
 #define XFIXES 1
 
 /* Build XDGA support */
-/* #undef XFreeXDGA */
+#undef XFreeXDGA
 
 /* Support Xinerama extension */
 #define XINERAMA 1
@@ -402,7 +393,7 @@
 /*#define XKB_IN_SERVER 1*/
 
 /* Vendor release */
-/* #undef XORG_RELEASE */
+#undef XORG_RELEASE
 
 /* Current Xorg version */
 #define XORG_VERSION_CURRENT (((1) * 10000000) + ((1) * 100000) + ((0) * 1000) + 0)
@@ -411,10 +402,10 @@
 #define XORG_DATE "10 Sept 2009"
 
 /* Build Xv Extension */
-/* #undef XvExtension */
+#undef XvExtension
 
 /* Build XvMC Extension */
-/* #undef XvMCExtension */
+#undef XvMCExtension
 
 /* Build XRes extension */
 #define XResExtension 1
@@ -425,11 +416,8 @@
 /* Support XTest extension */
 #define XTEST 1
 
-/* Support XTrap extension */
-/* #undef XTRAP */
-
 /* Support Xv extension */
-/* #undef XV */
+#undef XV
 
 /* Build APPGROUP extension */
 /* #undef XAPPGROUP */
@@ -441,15 +429,13 @@
 #define EVI 1
 
 /* Build Multibuffer extension */
-/* #undef MULTIBUFFER */
+#undef MULTIBUFFER
 
 /* Support DRI extension */
-/* #undef XF86DRI */
-
-/* #undef XEPHYR_DRI */
+#undef XF86DRI
 
 /* Build DRI2 extension */
-/* #undef DRI2 */
+#undef DRI2
 
 /* Build DBE support */
 #define DBE 1
@@ -474,70 +460,80 @@
 #endif
 
 /* Enable GNU and other extensions to the C environment for GLIBC */
-/* #undef _GNU_SOURCE */
+#undef _GNU_SOURCE
 
 /* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
+#undef const
 
 /* Define to `int' if <sys/types.h> does not define. */
-/* #undef pid_t */
+#undef pid_t
 
 /* Build Rootless code */
 #define ROOTLESS 1
 
 /* Define to 1 if unsigned long is 64 bits. */
-/* #undef _XSERVER64 */
+#undef _XSERVER64
 
 /* System is BSD-like */
-/* #undef CSRG_BASED */
+#undef CSRG_BASED
 
 /* Define to 1 if `struct sockaddr_in' has a `sin_len' member */
-/* #undef BSD44SOCKETS */
+#undef BSD44SOCKETS
 
 /* Define to 1 if modules should avoid the libcwrapper */
 #define NO_LIBCWRAPPER 1
 
 /* Support D-Bus */
-/* #undef HAVE_DBUS */
+#undef HAVE_DBUS
+
+/* Use D-Bus for input hotplug */
+#undef CONFIG_NEED_DBUS
 
 /* Support the D-Bus hotplug API */
-/* #undef CONFIG_DBUS_API */
+#undef CONFIG_DBUS_API
 
 /* Support HAL for hotplug */
-/* #undef CONFIG_HAL */
+#undef CONFIG_HAL
 
 /* Use only built-in fonts */
-/* #undef BUILTIN_FONTS */
-
-/* Avoid using font servers */
-/* #undef NOFONTSERVERACCESS */
+#undef BUILTIN_FONTS
 
 /* Use an empty root cursor */
-/* #undef NULL_ROOT_CURSOR */
+#undef NULL_ROOT_CURSOR
 
 /* Have a monotonic clock from clock_gettime() */
-/* #undef MONOTONIC_CLOCK */
+#undef MONOTONIC_CLOCK
 
 /* Define to 1 if the DTrace Xserver provider probes should be built in */
-/* #undef XSERVER_DTRACE */
+#undef XSERVER_DTRACE
 
 /* Define to 16-bit byteswap macro */
-/* #undef bswap_16 */
+#undef bswap_16
 
 /* Define to 32-bit byteswap macro */
-/* #undef bswap_32 */
+#undef bswap_32
 
 /* Define to 64-bit byteswap macro */
-/* #undef bswap_64 */
+#undef bswap_64
 
 /* Need the strcasecmp function. */
-/* #undef NEED_STRCASECMP */
+#undef NEED_STRCASECMP
 
 /* Need the strncasecmp function. */
-/* #undef NEED_STRNCASECMP */
+#undef NEED_STRNCASECMP
 
 /* Need the strcasestr function. */
 #define NEED_STRCASESTR 1
+
+/* Define to 1 if you have the `ffs' function. */
+#undef HAVE_FFS
+
+/* Correctly set _XSERVER64 for OSX fat binaries */
+#ifdef __APPLE__
+#include "dix-config-apple-verbatim.h"
+#endif
+
+#undef HAVE_AVC_NETLINK_ACQUIRE_FD
 
 #include <X11/Xwinsock.h>
 #include <X11/Xwindows.h>
