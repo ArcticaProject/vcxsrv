e34b0799856ac420d9401366514b2ed0ac20fa2b
marha <marha@users.sourceforge.net>
2009-09-28 09:54:29 +0000

    Solved compile warning.

--- xorg-server/dix/registry.c
+++ xorg-server/dix/registry.c
@@ -25,6 +25,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <X11/X.h>
 #include <X11/Xproto.h>
 #include "resource.h"
