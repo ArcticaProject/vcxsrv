d85e9f360392525f069cb89c0d3e97aab9d7d595
marha <marha@users.sourceforge.net>
2010-05-18 20:21:01 +0000

    Solved compile warnings

--- xorg-server/os/utils.c
+++ xorg-server/os/utils.c
@@ -122,6 +122,10 @@ __stdcall unsigned long GetTickCount(void);
 
 #include "picture.h"
 
+#ifdef _MSC_VER
+#define strdup _strdup
+#endif
+
 Bool noTestExtensions;
 #ifdef COMPOSITE
 Bool noCompositeExtension = FALSE;
