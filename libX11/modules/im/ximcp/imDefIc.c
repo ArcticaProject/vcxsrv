a0fa440dee24b4b39d30c1b04579691d42efba65
marha <marha@users.sourceforge.net>
2010-09-04 16:22:11 +0000

    libx11 git update 4/9/2010

--- libX11/modules/im/ximcp/imDefIc.c
+++ libX11/modules/im/ximcp/imDefIc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 1991, 1992 Sun Microsystems, Inc.  All rights reserved.
+ * Copyright 1991, 1992 Oracle and/or its affiliates. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
