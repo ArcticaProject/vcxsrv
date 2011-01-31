fd713bd3b716e9fa40964902ba17e50faac93400
marha <marha@users.sourceforge.net>
2011-01-31 09:03:13 +0000

    Solved problem of using an unitialised variable.

--- xorg-server/miext/rootless/rootlessScreen.c
+++ xorg-server/miext/rootless/rootlessScreen.c
@@ -325,7 +325,7 @@ RootlessGlyphs(CARD8 op, PicturePtr pSrc, PicturePtr pDst,
                 box.x1 = x - glyph->info.x;
                 box.y1 = y - glyph->info.y;
                 box.x2 = box.x1 + glyph->info.width;
-                box.y2 = box.y2 + glyph->info.height;
+                box.y2 = box.y1 + glyph->info.height;
 
                 x += glyph->info.xOff;
                 y += glyph->info.yOff;
