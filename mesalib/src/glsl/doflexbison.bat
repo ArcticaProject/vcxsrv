d2bdfe9f8d895ac64619f7bc2f7443ce886146e9
marha <marha@users.sourceforge.net>
2011-03-02 13:29:46 +0000

    Solved build problem in mesa because dependency checking in VS in not working like expected. Now build the lex and bison files in the same script.

--- mesalib/src/glsl/doflexbison.bat
+++ mesalib/src/glsl/doflexbison.bat
@@ -4,13 +4,18 @@ setlocal
 cd "%~dp0"
 
 set M4=..\..\..\tools\mhmake\m4.exe
-
 set BISON_PKGDATADIR=../../../tools/mhmake/src/bisondata
+
+set path=..\..\..\tools\mhmake;%path%
 
 ..\..\..\tools\mhmake\bison.exe -v -o glsl_parser.cpp -p "_mesa_glsl_" --defines=glsl_parser.h glsl_parser.yy
 
 ..\..\..\tools\mhmake\bison.exe -v -o glcpp/glcpp-parse.c --defines=glcpp/glcpp-parse.h glcpp/glcpp-parse.y
 
+copy "..\..\..\tools\mhmake\flex++.exe" flex.exe
+flex.exe --nounistd -oglsl_lexer.cpp glsl_lexer.ll
+flex.exe --nounistd -oglcpp/glcpp-lex.c glcpp/glcpp-lex.l
+del flex.exe
 
 endlocal
 
--- mesalib/src/mesa/program/doflexbison.bat
+++ mesalib/src/mesa/program/doflexbison.bat
@@ -4,10 +4,15 @@ setlocal
 cd "%~dp0"
 
 set M4=..\..\..\..\tools\mhmake\m4.exe
-
 set BISON_PKGDATADIR=../../../../tools/mhmake/src/bisondata
 
+set path=..\..\..\..\tools\mhmake;%path%
+
 ..\..\..\..\tools\mhmake\bison.exe -v -d --output=program_parse.tab.c program_parse.y
+
+copy "..\..\..\..\tools\mhmake\flex++.exe" flex.exe
+flex.exe --never-interactive --outfile=lex.yy.c program_lexer.l
+del flex.exe
 
 endlocal
 
