6272d6626e095565120b55d77ab6c1c84d269a81
marha <marha@users.sourceforge.net>
2011-03-08 08:42:23 +0000

    Added batch file to compile everything starting from a clean tree. Make sure path is OK to find all needed tools

---
+++ buildall.bat
@@ -0,0 +1,22 @@
+echo on
+devenv.com freetype\freetypevc10.sln /build "Release Multithreaded|Win32"
+devenv.com freetype\freetypevc10.sln /build "Debug Multithreaded|Win32"
+cd openssl
+perl Configure VC-WIN32
+call ms\do_nasm.bat
+nmake -f ms\nt.mak
+nmake DEBUG=1 -f ms\nt.mak
+cd ..\pthreads
+nmake VC-static
+nmake VC-static-debug
+cd ..
+devenv.com tools\mhmake\mhmakevc10.sln /build "Release|Win32"
+devenv.com tools\mhmake\mhmakevc10.sln /build "Debug|Win32"
+
+set MHMAKECONF=%~dp0
+
+..\tools\mhmake\release\mhmake.exe -C xorg-server MAKESERVER=1 DEBUG=1 vcxsrv_dbg.exe
+..\tools\mhmake\release\mhmake.exe -C xorg-server MAKESERVER=1
+
+cd xorg-server\installer
+call packageall.bat
