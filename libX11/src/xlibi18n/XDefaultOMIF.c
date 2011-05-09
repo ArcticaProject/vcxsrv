495e1bd0f33256db726731f949d2a95e75d106b8
marha <marha@users.sourceforge.net>
2011-05-09 11:50:19 +0000

    Solved compiler warning

--- libX11/src/xlibi18n/XDefaultOMIF.c
+++ libX11/src/xlibi18n/XDefaultOMIF.c
@@ -69,6 +69,7 @@ Sun Microsystems, Inc. or its licensors is granted.
 #include <X11/Xos.h>
 #include <X11/Xatom.h>
 #include <stdio.h>
+#include <unistd.h>
 #include <stdint.h>
 
 #define MAXFONTS		100
