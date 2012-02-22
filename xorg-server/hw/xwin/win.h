f0c81715e2d827fddc7251bac3cbe115ebd081d3
marha <marha@users.sourceforge.net>
2012-02-22 14:54:08 +0100

    Remove unused WindowExposuresProcPtr storage in screen privates

--- xorg-server/hw/xwin/win.h
+++ xorg-server/hw/xwin/win.h
@@ -556,7 +556,6 @@ typedef struct _winPrivScreenRec
   UnrealizeWindowProcPtr		UnrealizeWindow;
   ValidateTreeProcPtr			ValidateTree;
   PostValidateTreeProcPtr		PostValidateTree;
-  WindowExposuresProcPtr		WindowExposures;
   CopyWindowProcPtr			CopyWindow;
   ClearToBackgroundProcPtr		ClearToBackground;
   ClipNotifyProcPtr			ClipNotify;
