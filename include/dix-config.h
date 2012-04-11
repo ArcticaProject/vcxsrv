120bd371deb2b73a0bae5ce331511e4ea27bcff2
marha <marha@users.sourceforge.net>
2012-04-11 08:13:49 +0200

    Increased version to 1.12.0.1

--- include/dix-config.h
+++ include/dix-config.h
@@ -351,7 +351,7 @@
 #undef XORG_RELEASE
 
 /* Current Xorg version */
-#define XORG_VERSION_CURRENT (((1) * 10000000) + ((12) * 100000) + ((0) * 1000) + 0)
+#define XORG_VERSION_CURRENT (((1) * 10000000) + ((12) * 100000) + ((0) * 1000) + 1)
 
 /* Xorg release date */
 #define XORG_DATE "10 Sept 2009"
