567cbd4acc1e35ae40d99b24bb321d97632399db
marha <marha@users.sourceforge.net>
2012-06-11 13:56:12 +0200

    Fixed some minor mistakes in sync scripts

--- sync.bat
+++ sync.bat
@@ -1,21 +1,21 @@
-synchronise xserver ..\released\xorg-server --skip-dir=fonts.src --skip-dir=bitmaps --skip-dir=xkeyboard-config
-synchronise libxcb ..\released\libxcb
-synchronise xkeyboard-config ..\released\xorg-server\xkeyboard-config
-synchronise libX11 ..\released\libX11
-synchronise libXdmcp ..\released\libXdmcp
-synchronise libXext ..\released\libXext
-synchronise libfontenc ..\released\libfontenc
-synchronise libXinerama ..\released\libXinerama
-synchronise libXau ..\released\libXau
-synchronise xkbcomp ..\released\xkbcomp
-synchronise pixman ..\released\pixman
+synchronise -e xserver ..\released\xorg-server --skip-dir=fonts.src --skip-dir=bitmaps --skip-dir=xkeyboard-config
+synchronise -e libxcb ..\released\libxcb
+synchronise -e xkeyboard-config ..\released\xorg-server\xkeyboard-config
+synchronise -e libX11 ..\released\libX11
+synchronise -e libXdmcp ..\released\libXdmcp
+synchronise -e libXext ..\released\libXext
+synchronise -e libfontenc ..\released\libfontenc
+synchronise -e libXinerama ..\released\libXinerama
+synchronise -e libXau ..\released\libXau
+synchronise -e xkbcomp ..\released\xkbcomp
+synchronise -e pixman ..\released\pixman
 synchronise xextproto ..\released\X11\extensions --skip-dir=specs --skip-file=COPYING --skip-file=docbook.am --skip-file=autogen.sh --skip-file=configure.ac --skip-file=Makefile.am --skip-file=README --skip-file=.gitignore
 synchronise randrproto ..\released\X11\extensions --skip-file=autogen.sh --skip-file=configure.ac --skip-file=COPYING --skip-file=Makefile.am --skip-file=README --skip-file=.gitignore
-synchronise glproto            ..\released\gl
-synchronise mkfontscale        ..\released\mkfontscale
-synchronise xwininfo           ..\released\apps\xwininfo
-synchronise fontconfig         ..\released\fontconfig
-synchronise libXft             ..\released\libXft                                        
-synchronise libXmu             ..\released\libXmu
-synchronise libxtrans          ..\released\X11\xtrans
-synchronise mesa ..\released\mesalib --skip-dir=tests  --skip-dir=gtest --skip-dir=x86-64 --skip-dir=tnl_dd --skip-dir=sparc --skip-dir=tools --skip-dir=libdricore --skip-dir=x11 --skip-dir=osmesa --skip-dir=radeon --skip-dir=r200 --skip-dir=nouveau --skip-dir=intel --skip-dir=i965 --skip-dir=i915 --skip-dir=vgapi --skip-dir=shared-glapi --skip-dir=es1api  --skip-dir=es2api --skip-dir=gtest --skip-dir=glx --skip-dir=builtins --skip-dir=vl --skip-dir=gallium\docs --skip-dir=gallium\drivers --skip-dir=gallium\include --skip-dir=gallium\state_trackers --skip-dir=gallium\targets --skip-dir=gallium\winsys --skip-dir=gbm --skip-dir=getopt --skip-dir=egl --skip-dir=cso_cache --skip-dir=target-helpers --skip-dir=tgsi --skip-dir=translate --skip-dir=rtasm --skip-dir=pipebuffer --skip-dir=postprocess --skip-dir=rbug --skip-dir=pipe-loader --skip-dir=os --skip-dir=indices --skip-dir=gallivm --skip-dir=draw --skip-dir=pci_ids --skip-dir=doxygen --skip-dir=OLD --skip-dir=CL --skip-dir=c99
+synchronise -e glproto            ..\released\gl
+synchronise -e mkfontscale        ..\released\mkfontscale
+synchronise -e xwininfo           ..\released\apps\xwininfo
+synchronise -e fontconfig         ..\released\fontconfig
+synchronise -e libXft             ..\released\libXft                                        
+synchronise -e libXmu             ..\released\libXmu
+synchronise -e libxtrans          ..\released\X11\xtrans
+synchronise -e mesa ..\released\mesalib --skip-dir=tests  --skip-dir=gtest --skip-dir=x86-64 --skip-dir=tnl_dd --skip-dir=sparc --skip-dir=tools --skip-dir=libdricore --skip-dir=x11 --skip-dir=osmesa --skip-dir=radeon --skip-dir=r200 --skip-dir=nouveau --skip-dir=intel --skip-dir=i965 --skip-dir=i915 --skip-dir=vgapi --skip-dir=shared-glapi --skip-dir=es1api  --skip-dir=es2api --skip-dir=gtest --skip-dir=glx --skip-dir=builtins --skip-dir=vl --skip-dir=gallium\docs --skip-dir=gallium\drivers --skip-dir=gallium\include --skip-dir=gallium\state_trackers --skip-dir=gallium\targets --skip-dir=gallium\winsys --skip-dir=gbm --skip-dir=getopt --skip-dir=egl --skip-dir=cso_cache --skip-dir=target-helpers --skip-dir=tgsi --skip-dir=translate --skip-dir=rtasm --skip-dir=pipebuffer --skip-dir=postprocess --skip-dir=rbug --skip-dir=pipe-loader --skip-dir=os --skip-dir=indices --skip-dir=gallivm --skip-dir=draw --skip-dir=pci_ids --skip-dir=doxygen --skip-dir=OLD --skip-dir=CL --skip-dir=c99
