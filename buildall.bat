bc61f96ae3db76a9c65d679e15aaa36bd4a3e4a4
marha <marha@users.sourceforge.net>
2012-08-22 10:58:40 +0200

    Stop build process upon error of one of the commands

--- buildall.bat
+++ buildall.bat
@@ -1,26 +1,38 @@
 nasm -h >& nul
-if errorlevel NE 0 goto nasmerror
+if %errorlevel% NEQ 0 goto nasmerror
 
 echo on
 
 devenv.com freetype\freetypevc10.sln /build "Release Multithreaded|Win32"
+if %errorlevel% NEQ 0 goto end
 devenv.com freetype\freetypevc10.sln /build "Debug Multithreaded|Win32"
+if %errorlevel% NEQ 0 goto end
 cd openssl
 perl Configure VC-WIN32
+if %errorlevel% NEQ 0 goto end
 call ms\do_nasm.bat
+if %errorlevel% NEQ 0 goto end
 nmake -f ms\nt.mak
+if %errorlevel% NEQ 0 goto end
 nmake DEBUG=1 -f ms\nt.mak
+if %errorlevel% NEQ 0 goto end
 cd ..\pthreads
 nmake VC-static
+if %errorlevel% NEQ 0 goto end
 nmake VC-static-debug
+if %errorlevel% NEQ 0 goto end
 cd ..
 devenv.com tools\mhmake\mhmakevc10.sln /build "Release|Win32"
+if %errorlevel% NEQ 0 goto end
 devenv.com tools\mhmake\mhmakevc10.sln /build "Debug|Win32"
+if %errorlevel% NEQ 0 goto end
 
 set MHMAKECONF=%~dp0
 
 tools\mhmake\release\mhmake.exe -C xorg-server MAKESERVER=1 DEBUG=1 vcxsrv_dbg.exe
+if %errorlevel% NEQ 0 goto end
 tools\mhmake\release\mhmake.exe -C xorg-server MAKESERVER=1
+if %errorlevel% NEQ 0 goto end
 
 cd xorg-server\installer
 call packageall.bat
