821b51e814fe14d35f3807c525e4387022b9ed80
marha <marha@users.sourceforge.net>
2012-09-18 08:37:12 +0200

    Added -p option to bison command lines to avoid link problems

--- mesalib/src/glsl/doflexbison.bat
+++ mesalib/src/glsl/doflexbison.bat
@@ -10,7 +10,7 @@ set path=..\..\..\tools\mhmake;%path%
 
 ..\..\..\tools\mhmake\bison.exe -v -o glsl_parser.cpp -p "_mesa_glsl_" --defines=glsl_parser.h glsl_parser.yy
 
-..\..\..\tools\mhmake\bison.exe -v -o glcpp/glcpp-parse.c --defines=glcpp/glcpp-parse.h glcpp/glcpp-parse.y
+..\..\..\tools\mhmake\bison.exe -v -o glcpp/glcpp-parse.c -d -p "glcpp_parser_" --defines=glcpp/glcpp-parse.h glcpp/glcpp-parse.y
 
 copy "..\..\..\tools\mhmake\flex++.exe" flex.exe
 flex.exe --nounistd -oglsl_lexer.cpp glsl_lexer.ll
--- mesalib/src/mesa/program/doflexbison.bat
+++ mesalib/src/mesa/program/doflexbison.bat
@@ -8,7 +8,7 @@ set BISON_PKGDATADIR=../../../../tools/mhmake/src/bisondata
 
 set path=..\..\..\..\tools\mhmake;%path%
 
-..\..\..\..\tools\mhmake\bison.exe -v -d --output=program_parse.tab.c program_parse.y
+..\..\..\..\tools\mhmake\bison.exe -v -d -p "_mesa_program_" --output=program_parse.tab.c program_parse.y
 
 copy "..\..\..\..\tools\mhmake\flex++.exe" flex.exe
 flex.exe --never-interactive --outfile=lex.yy.c program_lexer.l
