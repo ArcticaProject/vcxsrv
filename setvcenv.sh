3691880b30d149d27b9116f48069cf6e33bb6ff9
Mike DePaulo <mikedep333@gmail.com>
2014-04-05 10:57:08 -0400

    Initial work to get VcXsrv to build with MSVC2012 xp compatibility, and for my build environment with cygwin bash.

--- setvcenv.sh
+++ setvcenv.sh
@@ -1,9 +1,15 @@
-export MHMAKECONF=`cygpath -da .`
+export MHMAKECONF=`cygpath -wa .`
 
-export VCINSTALLDIR="c:\Program Files\Microsoft Visual Studio 10.0\VC\\"
-export VS100COMNTOOLS="C:\Program Files\Microsoft Visual Studio 10.0\Common7\Tools\\"
-export VSINSTALLDIR="C:\Program Files\Microsoft Visual Studio 10.0\\"
-export WindowsSdkDir="C:\Program Files\Microsoft SDKs\Windows\v7.0A\\"
-export LIB="c:\Program Files\Microsoft Visual Studio 10.0\VC\lib;c:\Program Files\Microsoft SDKs\Windows\v7.0A\lib"
-export INCLUDE="c:\Program Files\Microsoft Visual Studio 10.0\VC\include;c:\Program Files\Microsoft SDKs\Windows\v7.0A\Include"
-export PATH="/vcxsrv:/cygdrive/c/Program Files/Microsoft Visual Studio 10.0/VC/bin:/cygdrive/c/Program Files/Microsoft Visual Studio 10.0/Common7/IDE:/cygdrive/c/Program Files/Microsoft SDKs/Windows/v7.0A/bin:$PATH"
+
+export VisualStudioVersion="11.0"
+
+export VCINSTALLDIR="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\\"
+export VS100COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\\"
+export VS110COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\\"
+export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 11.0\\"
+export WindowsSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0\\"
+export WindowsSdkDir_35="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\\"
+export WindowsSdkDir_old="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\\"
+export LIB="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\lib;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\lib"
+export INCLUDE="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\include;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include"
+export PATH="/vcxsrv:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE:/cygdrive/c/Program Files/Microsoft SDKs/Windows/v7.1A/bin:/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319:/cygdrive/c/Program Files (x86)/Windows Kits/8.0/bin/x86/:$PATH"
