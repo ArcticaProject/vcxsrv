1b493f424497599931b2abaca7c43925d6019558
Mike DePaulo <mikedep333@gmail.com>
2014-04-06 12:15:37 -0400

    Fix XP compatibility, bump date: 05 Apr 2014 -> 06 Apr 2014

--- openssl/util/pl/VC-32.pl
+++ openssl/util/pl/VC-32.pl
@@ -47,8 +47,8 @@ if ($FLAVOR =~ /WIN64/)
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
     $opt_cflags=$f.' /O2 /Ob2 /Oi /Ox /Oy /Ot /GL /Gy /GF /Zi';     
     $dbg_cflags=$f.'d /RTCc /RTC1 /Od /GS /GR /Gy /GF /Zi';   
-    $lflags="/NOLOGO /SUBSYSTEM:CONSOLE /OPT:REF /OPT:ICF /LTCG:STATUS";
-    $lflagsd="/NOLOGO /SUBSYSTEM:CONSOLE";
+    $lflags="/NOLOGO /SUBSYSTEM:CONSOLE,5.01 /OPT:REF /OPT:ICF /LTCG:STATUS";
+    $lflagsd="/NOLOGO /SUBSYSTEM:CONSOLE,5.01";
 
     *::perlasm_compile_target = sub {
 	my ($target,$source,$bname)=@_;
@@ -131,8 +131,8 @@ else	# Win32
     $lib_cflag='/Zl' if (!$shlib);	# remove /DEFAULTLIBs from static lib
     $opt_cflags=$f.' /O2 /Ob2 /Oi /Ox /Oy /Ot /GL /Gy /GF /Zi';     
     $dbg_cflags=$f.'d /RTCc /RTC1 /Od /GS /GR /Gy /GF /Zi';   
-    $lflags="/NOLOGO /SUBSYSTEM:CONSOLE /OPT:REF /OPT:ICF /LTCG:STATUS";
-    $lflagsd="/NOLOGO /SUBSYSTEM:CONSOLE";
+    $lflags="/NOLOGO /SUBSYSTEM:CONSOLE,5.01 /OPT:REF /OPT:ICF /LTCG:STATUS";
+    $lflagsd="/NOLOGO /SUBSYSTEM:CONSOLE,5.01";
     }
 $mlflags='';
 
--- setvcenv.sh
+++ setvcenv.sh
@@ -1,7 +1,12 @@
+# Reference for MSVC 2012 with XP Compatibility:
+# http://blogs.msdn.com/b/vcblog/archive/2012/10/08/10357555.aspx?PageIndex=2
+# Note that LINK is specified in various makefiles and scripts
+
 export MHMAKECONF=`cygpath -wa .`
 
-
 export VisualStudioVersion="11.0"
+
+export CL="/D_USING_V110_SDK71_"
 
 export VCINSTALLDIR="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\\"
 export VS100COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\\"
@@ -10,6 +15,8 @@ export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 11.0\\"
 export WindowsSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0\\"
 export WindowsSdkDir_35="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\\"
 export WindowsSdkDir_old="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\\"
-export LIB="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\lib;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\lib"
-export INCLUDE="c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\include;c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include"
-export PATH="/vcxsrv:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE:/cygdrive/c/Program Files/Microsoft SDKs/Windows/v7.1A/bin:/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319:/cygdrive/c/Program Files (x86)/Windows Kits/8.0/bin/x86/:$PATH"
+export WindowsSdkDir_71A="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\bin\\"
+export LIB="c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\lib;c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\lib"
+export INCLUDE="c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include;c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\include"
+
+export PATH="/cygdrive/c/Program Files/Microsoft SDKs/Windows/v7.1A/Bin:/vcxsrv:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/VC/bin:/cygdrive/c/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE:/cygdrive/c/Windows/Microsoft.NET/Framework/v4.0.30319:/cygdrive/c/Program Files (x86)/Windows Kits/8.0/bin/x86/:$PATH"
