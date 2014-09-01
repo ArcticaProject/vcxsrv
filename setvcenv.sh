b599637897f1cf7a98c59d37ff9f24e83f05f5bb
Mike DePaulo <mikedep333@gmail.com>
2014-09-01 14:02:54 -0400

    Fix build of mhmake

--- setvcenv.sh
+++ setvcenv.sh
@@ -6,18 +6,16 @@ export MHMAKECONF=`cygpath -wa .`
 
 export VisualStudioVersion="12.0"
 
-export CL="/D_USING_V110_SDK71_"
-
 export VCINSTALLDIR="c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\\"
 export VS100COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\\"
 export VS110COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\Tools\\"
 export VS120COMNTOOLS="C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\Tools\\"
 export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio 12.0\\"
-export WindowsSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\\"
-export WindowsSdkDir_35="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\bin\\"
-export WindowsSdkDir_old="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\bin\\"
-export WindowsSdkDir_71A="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\bin\\"
-export WindowsSdkDir_80="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0\bin\\"
+export WindowsSdkDir="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\\"
+export WindowsSdkDir_35="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A\\"
+export WindowsSdkDir_old="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1\\"
+export WindowsSdkDir_71A="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\\"
+export WindowsSdkDir_80="C:\Program Files (x86)\Microsoft SDKs\Windows\v8.0\\"
 export LIB="c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\lib;c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\lib"
 export INCLUDE="c:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include;c:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\include"
 
